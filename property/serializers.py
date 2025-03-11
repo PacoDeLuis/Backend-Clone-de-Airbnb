4e5261817f12f367c0b200ae5dff2b1041174afb
Paco De Luis <ponchoflores464@gmail.com>
2025-03-11 11:49:49 -0600

    ded

---
+++ property/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from .models import Property, Reservation
+
+
+class PropertiesListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Property
+        fields = (
+            'id',
+            'title',
+            'price_per_night',
+            'image_url',
+        )
+        
+class ReservationsListSerializer(serializers.ModelSerializer):
+    property = PropertiesListSerializer(read_only=True, many=False)
+    
+    class Meta:
+        model = Reservation
+        fields = (
+            'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
+        )
+
+
