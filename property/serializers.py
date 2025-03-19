cc75295ea472bffddcf81341b74ccda55fe46756
Paco De Luis <ponchoflores464@gmail.com>
2025-03-18 18:12:58 -0600

    book property

--- property/serializers.py
+++ property/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from .models import Property, Reservation
 
+from useraccount.serializers import UserDetailSerializer
+
 
 class PropertiesListSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,7 +14,26 @@ class PropertiesListSerializer(serializers.ModelSerializer):
             'price_per_night',
             'image_url',
         )
-        
+
+
+class PropertiesDetailSerializer(serializers.ModelSerializer):
+    landlord = UserDetailSerializer(read_only=True, many=False)
+
+    class Meta:
+        model = Property
+        fields = (
+            'id',
+            'title',
+            'description',
+            'price_per_night',
+            'image_url',
+            'bedrooms',
+            'bathrooms',
+            'guests',
+            'landlord'
+        )
+
+
 class ReservationsListSerializer(serializers.ModelSerializer):
     property = PropertiesListSerializer(read_only=True, many=False)
     
@@ -21,5 +42,3 @@ class ReservationsListSerializer(serializers.ModelSerializer):
         fields = (
             'id', 'start_date', 'end_date', 'number_of_nights', 'total_price', 'property'
         )
-
-
